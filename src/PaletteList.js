68a3353f48adbbbf9402ad5bcebafbb7b4f9e27d
Avinash <avinash.lahel@team.telstra.com>
2019-06-19 17:40:42 +0530

    Completed miniPalette (landing page)

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,16 +1,63 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom'
+import MiniPalette from './MiniPalette'
+import {withStyles} from "@material-ui/styles";
+
+const styles = {
+    root: {
+        backgroundColor: 'blue',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'flex-start'
+    },
+    container: {
+        display: 'flex',
+        width: '50%',
+        alignItems: 'flex-start',
+        flexDirection: 'column',
+        flexWrap: 'wrap',
+        // border: '1px solid white'
+    },
+    nav: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        width: '100%',
+        color: 'white'
+    },
+    palettes: {
+        boxSizing: 'border-box',
+        width : '100%',
+        display: 'grid',
+        gridTemplateColumns : 'repeat(3,30%)',
+        gridGap: '5%'
+    }
+};
+
 
 class PaletteList extends Component {
+
+
     render() {
-        const {palettes} = this.props;
+        const {palettes, classes} = this.props;
 
         return (
-                palettes.map(palette => {
-                    return <p><Link to={`palette/${palette.id}`}>{palette.paletteName}</Link></p>
-                })
+            <div className={classes.root}>
+                <div className={classes.container}>
+                    <nav className={classes.nav}>
+                        <h1>React Colors</h1>
+                    </nav>
+                    <div className={classes.palettes}>
+                        {palettes.map(palette => {
+                            return (<MiniPalette  {...palette}/>)
+                        })}
+
+                    </div>
+                </div>
+
+            </div>
         );
     }
 }
 
-export default PaletteList;
+export default withStyles(styles)(PaletteList);
